d3b784f7c89f04cf801a40848931ae7db5eaf2ae
Dinesh Reddy Gali <128059225+Dinesh-Reddy10@users.noreply.github.com>
2025-07-25 08:16:38 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/books', require('./routes/bookRoutes'));
+app.use('/api/reviews', require('./routes/reviewRoutes'));
+
+
+console.log("Connecting to:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
